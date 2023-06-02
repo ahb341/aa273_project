f081ef115c484806648885357b08fd77981c9782
ahb341 <36087442+ahb341@users.noreply.github.com>
2023-06-01 22:31:03 -0700

    Load data for centralized

--- Aaron/Multi-Robot-Centralized/load_data.m
+++ Aaron/Multi-Robot-Centralized/load_data.m
@@ -22,19 +22,22 @@ n_landmarks = length(Landmark_Groundtruth);
 % landmark 1-15 id
 IDList = [72,27,54,70,36,18,25,9,81,16,90,61,45,7,63];
 
+% First, read data
 for i=1:n_robots
     % read groundtruth
     disp(['Reading robot ' num2str(i) ' groundtruth'])
     [gt x y theta] = textread([dir 'Robot' num2str(i) '_Groundtruth.dat'], '%f %f %f %f','commentstyle','shell');
     eval(['Robot' num2str(i) '_Groundtruth = [gt-gt(1) x y theta];']); 
     clear x y theta;
-    
+
     % read odometry
     disp(['Reading robot ' num2str(i) ' odometry'])
     [time, v, w] = textread([dir 'Robot' num2str(i) '_Odometry.dat'], '%f %f %f','commentstyle','shell');
     eval(['Robot' num2str(i) '_Odometry = [time-gt(1) v w];']);
     clear time v w;
-    
+    eval(['u' num2str(i) ' = Robot' num2str(i) '_Odometry;']);
+    eval(['u' num2str(i) '_sz = size(u' num2str(i) ');']);
+
     % read measurements
     disp(['Reading robot ' num2str(i) ' measurements'])
     [time, barcode_num, r b] = textread([dir 'Robot' num2str(i) '_Measurement.dat'], '%f %f %f %f','commentstyle','shell');
@@ -50,18 +53,52 @@ for i=1:n_robots
         len = eval(['length(Robot' num2str(i) '_Measurement)']);
     end
     clear time barcode_num r b gt len;
+end
 
-    disp(['Calculating robot ' num2str(i) ' covariance'])
-    eval(['[N,~] = size(Robot' num2str(i) '_Groundtruth);']);
-    eval(['xarr = Robot' num2str(i) '_Groundtruth(:,2);']); 
-    eval(['yarr = Robot' num2str(i) '_Groundtruth(:,3);']);
-    eval(['tharr = Robot' num2str(i) '_Groundtruth(:,4);']);
-    eval(['Q' num2str(i) ' = calcQ(xarr,yarr,tharr,N);']);
-    eval(['[N,~] = size(Robot' num2str(i) '_Measurement);']);
-    eval(['rarr = Robot' num2str(i) '_Measurement(:,3);']);
-    eval(['barr = Robot' num2str(i) '_Measurement(:,4);']);
-    eval(['R' num2str(i) ' = calcR(rarr,barr,N);']);
-    clear N xarr yarr tharr rarr barr
+% Second, adjust odometry 
+% [timestamp, v, omega, robot #]
+% odo = zeros(u1_sz(1) + u2_sz(1) + u3_sz(1) + u4_sz(1) + u5_sz(1), ...
+%     1+u1_sz(2));
+% nxt = 1;
+% for i = 1:n_robots
+%     sz = eval(['u' num2str(i) '_sz(1);']);
+%     u = eval(['u' num2str(i) ';']);
+%     odo(nxt:nxt+sz-1,:) = [u i*ones(sz,1)];
+%     nxt = nxt+sz;
+% end
+% clear u sz nxt
+% odo = sortrows(odo);
+% now copy odometry so there is an input at each time step
+% UPDATE: handle the discrepancy in the simulation loop
+% for i = 1:n_robots
+%     last_control = [0 0];
+%     j = 1;
+%     while j < size(odo,1)
+%         % copy the last control input
+%         if odo(j,4) == i
+%             last_control = odo(j,2:3);
+%             j = j+1;
+%         else
+%             t = odo(j,1);
+%             while j < size(odo,1) && odo(j,1) == t && odo(j,4) ~= i
+%                 % see if we already have a matching time for our robot
+%                 j = j+1;
+%             end
+%             if odo(j,1) ~= t
+%                 % no matching time so insert last control
+%                 odo = [odo(1:j,:); [odo(j,1) last_control i]; odo(j+1:end,:)];
+%             elseif odo(j,4) == i
+%                 % matching time, update last control
+%                 last_control = odo(j,2:3);
+%             end
+%         end
+% 
+%     end
+% end
+
+
+% finally, merge data
+for i=1:n_robots
 
     % merge data
     disp(['Merging robot ' num2str(i) ' data'])
@@ -90,6 +127,26 @@ for i=1:n_robots
     % If an odometry, indicator column will be =0,
             % info1, info2 columns will be populated. Last column will be NaN
     eval(['Robot' num2str(i) '=sortrows(Robot' num2str(i) ');']);
+
+    k = 1;
+    Robot = eval(['Robot' num2str(i)]);
+    while k < size(Robot,1)
+        % Set next timestep after a set of measurements as previously recorded input
+        j = k;
+        if Robot(j,2) == 1
+            Robot(j,3) = find(IDList==Robot(j,3));
+            while Robot(j+1,2) == 1 && Robot(k,1) == Robot(j+1,1)
+                j = j+ 1;
+                Robot(j,3) = find(IDList==Robot(j,3));
+            end
+            control = Robot(k-1,:);
+            control(1,1) = Robot(j,1);
+            Robot = [Robot(1:j,:);control;Robot(j+1:end,:)];
+            k = j;
+        end
+        k = k+1;
+    end
+    eval(['Robot' num2str(i) '=Robot;']);
 end
-clear i dir
+clear i dir Robot j k u
 disp('Parsing Complete')
